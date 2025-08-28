fae434ba5d7258af0e63baa740ed9b138e7651d9
Zaryab Zubair <zaryabz043@gmail.com>
2025-08-28 13:00:53 +0500

    learned path-modules

---
+++ PartOne/01_node-tutorial/04_path-module/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+console.log('Directory Name', path.dirname(__filename));
+
+console.log('File Name', path.basename(__filename));
+
+console.log('File Extension', path.extname(__filename));const joinPath = path.join("/user", "documents", "node", "projects");
+
+console.log("Joined path", joinPath);
+
+const resolvePath = path.resolve("user", "documents", "node", "project");
+console.log("Resolve path:", resolvePath);
+
+const normalizePath = path.normalize("/user/.documents/../node/projects");
+console.log("normalizePath", normalizePath);
