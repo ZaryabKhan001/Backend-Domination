82710b4535e17e3fa92fcbfaf49e9f4d891dfda5
Zaryab Zubair <zaryabz043@gmail.com>
2025-09-02 11:10:22 +0500

    mongodb basics completed

--- PartOne/05_mongodb-basics/index.js
+++ PartOne/05_mongodb-basics/index.js
@@ -1,31 +1,86 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const express = require('express');
 
-const connectDb = async () => {
-  try {
-    const connect = await mongoose.connect(
-      'mongodb+srv://zaryabzubair_db_user:CzseBq98mMnRDj3h@cluster0.zff7etw.mongodb.net/'
-    );
-    if (connect.connection) {
-      console.log('DB Connected Successfully');
-    }
-  } catch (error) {
-    console.log(`DB Connection Failed ${error}`);
-    process.exit(1);
-  }
-};
-connectDb();
+const app = express();
+const port = process.env.PORT || 3001;
 
-const userSchema = new mongoose.Schema(
-  {
-    name: String,
-    email: String,
-    age: Number,
-    isActive: Boolean,
-    tags: [String],
-  },
-  { timestamps: true }
-);
+dotenv.config();
 
-const User = mongoose.model('User', userSchema);
+//* Db connection
+// const connectDb = async () => {
+//   try {
+//     const connect = await mongoose.connect(process.env.MONGODB_URI);
+//     if (connect.connection) {
+//       console.log('DB Connected Successfully');
+//     }
+//   } catch (error) {
+//     console.log(`DB Connection Failed ${error}`);
+//     process.exit(1);
+//   }
+// };
+// connectDb();
 
-export default User;
+//* User schema
+// const userSchema = new mongoose.Schema(
+//   {
+//     name: String,
+//     email: String,
+//     age: Number,
+//     isActive: Boolean,
+//     tags: [String],
+//   },
+//   { timestamps: true }
+// );
+
+// const User = mongoose.model('User', userSchema);
+
+// const runQueryExample = async () => {
+//   try {
+//     const newUser = await User.create({
+//       name: 'Zaryab',
+//       email: 'zaryabz043@gmail.com',
+//       age: 22,
+//       isActive: true,
+//       tags: ['developer', 'designer'],
+//     });
+//   } catch (error) {
+//     console.log('Error creating User', error);
+//   }
+// };
+// runQueryExample();
+
+//* get all users
+// const allUsers = await User.find({});
+
+//* get all users who are active
+// const allUsers = await User.find({isActive: true});
+
+//* first user which is active
+// const allUsers = await User.findOne({isActive: true});
+
+//* update one user
+// db.users.updateOne(
+//   { email: "zaryab@example.com" },
+//   { $set: { name: "New Name" } }
+// )
+
+//* update many users
+// db.users.updateMany(
+//   { role: "guest" },
+//   { $set: { name: "Updated Guest" } }
+// )
+
+//* delete one
+// db.users.deleteOne(
+// { email: "zaryab@example.com" }
+// )
+
+//* delete many
+// db.users.deleteMany(
+//   { role: "guest" }
+// )
+
+app.listen(port, () => {
+  console.log(`App is listening on Port: ${port}`);
+});
