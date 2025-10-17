3f38bde2d8431ccc6bfc7219a9de901a3698ec56
Zaryab Zubair <zaryabz043@gmail.com>
2025-10-17 15:42:25 +0500

    worked on user registration functionality

---
+++ PartTwo/04_microservices/identity-service/src/models/refreshToken.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema, model } from 'mongoose';
+
+const refreshTokenSchema = new Schema(
+  {
+    token: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      unique: true,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+refreshTokenSchema.index({expiresAt: 1}, {expireAfterSeconds: 0});
+
+const RefreshToken = model('RefreshToken', refreshTokenSchema);
+
+export default RefreshToken;
---
+++ PartTwo/04_microservices/identity-service/src/models/user.model.js
@@ -0,0 +1,48 @@
+import { Schema, model } from 'mongoose';
+import argon2 from 'argon2';
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    try {
+      this.password = await argon2.hash(this.password);
+    } catch (error) {
+      return next(error);
+    }
+  }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    return await argon2.verify(this.password, candidatePassword);
+  } catch (error) {
+    throw error;
+  }
+};
+
+userSchema.index({ username: 'text' });
+
+const User = model('User', userSchema);
+
+export default User;
