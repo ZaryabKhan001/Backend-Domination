5491e0e466c724456a7f837738440274a95b8cc4
Zaryab Zubair <zaryabz043@gmail.com>
2025-10-24 15:06:12 +0500

    enabled new routes

--- PartTwo/04_microservices/post-service/src/routes/post.route.js
+++ PartTwo/04_microservices/post-service/src/routes/post.route.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { handleCreatePost } from '../controllers/post.controller.js';
+import { handleCreatePost, handleGetAllPosts, handleGetPostById, handleDeletePost } from '../controllers/post.controller.js';
 import { authenticateRequest } from '../middlewares/auth.middleware.js';
 import { rateLimiter } from '../middlewares/rateLimitng.middleware.js';
 const router = express.Router();
 
 router.use(authenticateRequest);
-router.post('/create', rateLimiter(1 * 60 * 1000, 10), handleCreatePost);
+router.post('/create', rateLimiter(1 * 60 * 1000, 50), handleCreatePost);
+router.get('/all-posts', rateLimiter(1 * 60 * 1000, 100), handleGetAllPosts);
+router.get('/:id', rateLimiter(1 * 60 * 1000, 100), handleGetPostById);
+router.delete('/:id', rateLimiter(1 * 60 * 1000, 50), handleDeletePost);
 
 export default router;
