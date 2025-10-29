b0bd9c5f990b83759c8fd2ba1d7514732b5c00ba
Zaryab Zubair <zaryabz043@gmail.com>
2025-10-29 17:16:19 +0500

    [search-service] rabbitmq setup and db connection

--- PartTwo/04_microservices/search-service/src/utils/rabbitmq.js
+++ PartTwo/04_microservices/search-service/src/utils/rabbitmq.js
@@ -4,39 +4,39 @@ import { logger } from './logger.js';
 let connection = null;
 let channel = null;
 
-const EXCHANGE_NAME = 'post_events';
+let EXCHANGE_NAME = 'post_events';
 
 export const connectRabbitMQ = async () => {
   if (channel) return channel;
+
   try {
     connection = await amqplib.connect(process.env.RABBITMQ_URL);
     logger.info('Connected to RabbitMQ');
 
     channel = await connection.createChannel();
-    logger.info('Channel Created');
+    logger.info('Channel created');
 
     await channel.assertExchange(EXCHANGE_NAME, 'topic', { durable: true });
 
-    // reconnecting on closing
+    // reconnecting on disconnecting
     connection.on('close', () => {
-      logger.warn('RabbitMQ Connection Closed');
+      logger.warn('RabbitMQ Connection closed');
       connection = null;
       channel = null;
       setTimeout(connectRabbitMQ, 5000);
     });
 
     // error handling
-    connection.on('error', (err) => {
-      logger.warn('Error Connecting to RabbitMQ', err?.message);
+    connection.on('error', (error) => {
+      logger.error('Error Connecting to RabbitMQ', error?.message);
     });
 
-    // Gracefully shutting down
     ['SIGINT', 'SIGTERM'].forEach((signal) => {
       process.on(signal, async () => {
         try {
           logger.info('Closing RabbitMQ connection...');
-          await channel?.close();
           await connection?.close();
+          await channel?.close();
           process.exit(0);
         } catch (error) {
           logger.error(
@@ -47,10 +47,9 @@ export const connectRabbitMQ = async () => {
         }
       });
     });
-
     return channel;
   } catch (error) {
-    logger.error('Failed to connect to RabbitMQ');
+    logger.error('Failed to connect to RabbitMQ', error);
   }
 };
 
@@ -58,11 +57,29 @@ export const publishEvent = async (routingKey, message) => {
   if (!channel) {
     await connectRabbitMQ();
   }
+
   channel.publish(
     EXCHANGE_NAME,
     routingKey,
     Buffer.from(JSON.stringify(message)),
-     { persistent: true }
+    { persistent: true }
   );
-  logger.info(`${routingKey}: Event published`);
+};
+
+export const consumeEvent = async (queueName, routingKey, callback) => {
+  if (!channel) {
+    await connectRabbitMQ();
+  }
+  await channel.assertQueue(queueName, { durable: true });
+  await channel.bindQueue(queueName, EXCHANGE_NAME, routingKey);
+
+  channel.consume(queueName, async (msg) => {
+    if (msg !== null) {
+      const content = JSON.parse(msg.content.toString());
+      await callback(content);
+      channel.ack(msg); // remove msg from queue
+    }
+  });
+
+  logger.info(`Queue "${queueName}" subscribed to "${routingKey}"`);
 };
