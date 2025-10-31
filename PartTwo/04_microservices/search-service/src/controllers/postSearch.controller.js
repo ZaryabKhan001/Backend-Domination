f913b0b9ecb1bae9497d2fb8363f86c578e8a0b0
Zaryab Zubair <zaryabz043@gmail.com>
2025-10-31 18:03:13 +0500

    completed elastic search

--- PartTwo/04_microservices/search-service/src/controllers/postSearch.controller.js
+++ PartTwo/04_microservices/search-service/src/controllers/postSearch.controller.js
@@ -1,28 +1,76 @@
 import { logger } from '../utils/logger.js';
-import PostSearch from '../models/postSearch.model.js';
+import { searchPost } from '../utils/es.utils.js';
+// import PostSearch from '../models/postSearch.model.js';
 
+//? mongodb search
+// export const handleSearchPost = async (req, res) => {
+//   logger.info('Post Search Endpoint hit...');
+//   try {
+//     const { query } = req.query;
+
+//     const results = await PostSearch.find(
+//       {
+//         $text: { $search: query },
+//       },
+//       {
+//         score: { $meta: 'textScore' },
+//       }
+//     )
+//       .sort({ score: { $meta: 'textScore' } })
+//       .limit(10);
+
+//     logger.info('Post Searched Successfully');
+
+//     return res.status(200).json({
+//       success: true,
+//       message: 'Search Results fetched successfully',
+//       results: results,
+//     });
+//   } catch (error) {
+//     logger.error('Post Searched Failed', error);
+//     return res.status(500).json({
+//       success: false,
+//       message: error?.message || 'Search post Failed!',
+//     });
+//   }
+// };
+
+//? elastic search
 export const handleSearchPost = async (req, res) => {
   logger.info('Post Search Endpoint hit...');
   try {
-    const { query } = req.query;
+    const { query, page = 1, limit = 5 } = req.query;
 
-    const results = await PostSearch.find(
-      {
-        $text: { $search: query },
+    if (!query) {
+      return res.status(400).json({
+        success: false,
+        message: 'Query parameter is required',
+      });
+    }
+
+    const from = (page - 1) * limit;
+
+    const esQuery = {
+      match: {
+        content: query,
       },
-      {
-        score: { $meta: 'textScore' },
-      }
-    )
-      .sort({ score: { $meta: 'textScore' } })
-      .limit(10);
+    };
+
+    const options = {
+      from: from,
+      size: Number(limit),
+      sort: [{ createdAt: 'desc' }],
+    };
+
+    const { total, hits } = await searchPost('posts', esQuery, options);
 
     logger.info('Post Searched Successfully');
 
     return res.status(200).json({
       success: true,
       message: 'Search Results fetched successfully',
-      results: results,
+      totalMatches: total,
+      posts: hits,
     });
   } catch (error) {
     logger.error('Post Searched Failed', error);
--- PartTwo/04_microservices/search-service/src/events/post.events.js
+++ PartTwo/04_microservices/search-service/src/events/post.events.js
@@ -1,17 +1,56 @@
 import { logger } from '../utils/logger.js';
-import PostSearch from '../models/postSearch.model.js';
+// import PostSearch from '../models/postSearch.model.js';
+import * as esUtils from '../utils/es.utils.js';
 
+//? mongodb
+// export const handleCreatePost = async (event) => {
+//   logger.info('Post Creation for Search Started.');
+//   try {
+//     const { postId, content, userId } = event;
+
+//     await PostSearch.create({
+//       postId,
+//       content,
+//       userId,
+//       createdAt: Date.now(),
+//     });
+//     logger.info('Post Created Successfully for Search');
+//   } catch (error) {
+//     logger.error('Post Creation for Search Failed!', error);
+//   }
+// };
+
+// export const handleDeletePost = async (event) => {
+//   logger.info('Post Deletion from Search Started');
+//   try {
+//     const { postId } = event;
+//     await PostSearch.findOneAndDelete({ postId: postId });
+//     logger.info('Post Deleted Successfully from Search');
+//   } catch (error) {
+//     logger.error('Post Deletion from Search Failed!', error);
+//   }
+// };
+
+//? elastic search
 export const handleCreatePost = async (event) => {
   logger.info('Post Creation for Search Started.');
   try {
     const { postId, content, userId } = event;
 
-    await PostSearch.create({
-      postId,
-      content,
-      userId,
-      createdAt: Date.now(),
-    });
+    const result = await esUtils.addDocument(
+      'posts',
+      {
+        postId: postId,
+        userId: userId,
+        content: content,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+      },
+      postId
+    );
+    if (!result) {
+      throw new Error('Error in Post Creation for Search.');
+    }
     logger.info('Post Created Successfully for Search');
   } catch (error) {
     logger.error('Post Creation for Search Failed!', error);
@@ -22,7 +61,13 @@ export const handleDeletePost = async (event) => {
   logger.info('Post Deletion from Search Started');
   try {
     const { postId } = event;
-    await PostSearch.findOneAndDelete({ postId: postId });
+
+    const result = await esUtils.deleteDocument('posts', postId);
+
+    if (!result) {
+      throw new Error('Error in Post Deletion from Search.');
+    }
+
     logger.info('Post Deleted Successfully from Search');
   } catch (error) {
     logger.error('Post Deletion from Search Failed!', error);
